7281d5af10c40f52ae50272a4809657b8b297719
Marcin Król <marcinkrol1024@gmail.com>
2017-10-22 14:50:14 +0200

    Add filters usage

--- src/main/java/test/TestRead.java
+++ src/main/java/test/TestRead.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import static com.mongodb.client.model.Filters.eq;
 import static io.vavr.API.List;
 
 public class TestRead {
@@ -16,6 +17,9 @@ public class TestRead {
         .forEach(System.out::println);
 
     List().appendAll(mongoCollection.find(Document.parse("{\"name\": \"shopname\"}")))
+        .forEach(System.out::println);
+
+    List().appendAll(mongoCollection.find(eq("name", "shopname")))
         .forEach(System.out::println);
   }
 }

fbb534df2499ee32df22ca19266225a40757cc53
Marcin Król <marcinkrol1024@gmail.com>
2017-10-22 15:59:57 +0200

    Add read

--- src/main/java/test/TestRead.java
+++ src/main/java/test/TestRead.java
@@ -3,6 +3,7 @@ package test;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import io.vavr.collection.List;
 import org.bson.Document;
 
 import static com.mongodb.client.model.Filters.eq;
@@ -12,14 +13,14 @@ public class TestRead {
   public static void main(String[] args) {
     MongoClient mongoClient = new MongoClient("localhost", 27017);
     MongoDatabase mongoDatabase = mongoClient.getDatabase("shops");
-    MongoCollection mongoCollection = mongoDatabase.getCollection("shops");
+    MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("shops", Document.class);
     List().appendAll(mongoCollection.find())
         .forEach(System.out::println);
 
     List().appendAll(mongoCollection.find(Document.parse("{\"name\": \"shopname\"}")))
         .forEach(System.out::println);
 
-    List().appendAll(mongoCollection.find(eq("name", "shopname")))
+    List.ofAll(mongoCollection.find(eq("name", "shopname")))
         .forEach(System.out::println);
   }
 }
